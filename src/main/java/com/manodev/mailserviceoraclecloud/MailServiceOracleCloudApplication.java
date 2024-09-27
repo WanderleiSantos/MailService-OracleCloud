15d147bce91761365ee6fa32fb09485dd1495ce5
Wanderlei Santos <wanderleisantosdias@gmail.com>
2024-09-26 23:07:57 -0300

    first commit

---
+++ src/main/java/com/manodev/mailserviceoraclecloud/MailServiceOracleCloudApplication.java
@@ -0,0 +1,13 @@
+package com.manodev.mailserviceoraclecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceOracleCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailServiceOracleCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manodev/mailserviceoraclecloud/MailServiceOracleCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manodev.mailserviceoraclecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceOracleCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
